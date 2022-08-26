92978be9ce6ab08768242dccd0e94bbce441b558
EkaterinaShulga <shulga23@gmail.com>
2022-08-26 16:01:00 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/RestfulApplicationHogwartsApplication.java
@@ -0,0 +1,14 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class RestfulApplicationHogwartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulApplicationHogwartsApplication.class, args);
+    }
+
+}
